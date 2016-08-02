c6d005bbb2685fc034381d3b7d9480c5183cf818
Hiroyuki Kai <hk.biomems@gmail.com>
2016-08-02 10:26:55 +0900

    PlotCV

--- src/plot_cv.py
+++ src/plot_cv.py
@@ -1,41 +1,34 @@
 import luigi
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
-from data_tools import ambiguous_path, colors10
-from measure_cie_space import CollectCIESpace
 from figure_tools import figure
 from luigi_tools import cleanup
 import matplotlib.pyplot as plt
-import os
 import numpy as np
-from matplotlib import colors
 
 
-@figure('1c')
+@figure('2a', show=False)
 def plot_cv():
-    for i, rate in enumerate([5, 10, 50, 80, 100, 200]):
-        path = os.path.join('../../Suda Electrochromism data/20160513 CV/', '70wt%% CV mem %dmV.TXT' % rate)
-        print(path)
-        vs2 = []
-        with open(path, 'rU') as f:
-            s = ''
-            while s.rstrip() != 'Potential/V\tCurrent/A':
-                s = f.readline()
-            f.readline()  # Skip a blank line
-            for line in f:
-                ws = line.split('\t')
-                if len(ws) == 2:
-                    vs2.append(map(float, ws))
-        arr = np.array(vs2)
-        arr[:, 1] = arr[:, 1] * 1e6
-        idxs = np.argwhere(arr[:, 0] == 0)
-        idx1 = idxs[-3][0]
-        idx2 = idxs[-1][0]
-        v = [0.2, 0.4, 0.5, 0.6, 0.8, 1][i]
-        plt.plot(arr[idx1:idx2, 0], arr[idx1:idx2, 1], c=colors.hsv_to_rgb((1, 1, v)))
+    path = os.path.join('../data/2000 rpm CV.txt')
+    print(path)
+    vs2 = []
+    with open(path, 'rU') as f:
+        s = ''
+        while s.find('Potential/V') != 0:
+            s = f.readline()
+        f.readline()  # Skip a blank line
+        for line in f:
+            ws = line.split('\t')
+            if len(ws) >= 2:
+                vs2.append(map(float, ws))
+    print(vs2)
+    arr = np.array(vs2)
+    arr[:, 1] = arr[:, 1] * 1e6
+    idxs = np.argwhere(arr[:, 0] == 0)
+    idx1 = idxs[-3][0]
+    idx2 = idxs[-1][0]
+    plt.plot(arr[idx1:idx2, 0], arr[idx1:idx2, 1], c='b')
     plt.xlim([-0.8, 1])
-    plt.ylim([-600, 600])
+
+    plt.ylim([-200, 200])
 
 
 class PlotCV(luigi.Task):
