6c2019330c1e4e028001452e0a938c0aed87ae09
Hiroyuki Kai <hk.biomems@gmail.com>
2016-11-02 13:27:39 +0900

    Reorder l values for plotting

--- src/measure_calibration_lab.py
+++ src/measure_calibration_lab.py
@@ -12,6 +12,7 @@ from scipy.optimize import curve_fit
 from util import ensure_exists, basename_noext
 import pandas as pd
 from image_tools import get_cie_rois
+from skimage import color
 
 
 def read_csv(csv_path):
@@ -34,6 +35,60 @@ def read_csv(csv_path):
             rs.append(PointROI(v[i * 4:i * 4 + 4]))
         rois[k] = rs
     return rois
+
+
+chart_colors_str = """735244
+c29682
+627a9d
+576c43
+8580b1
+67bdaa
+d67e2c
+505ba6
+c15a63
+5e3c6c
+9dbc40
+e0a32e
+383d96
+469449
+af363c
+e7c71f
+bb5695
+0885a1
+f3f3f2
+c8c8c8
+a0a0a0
+7a7a79
+555555
+343434"""
+
+
+def extract_l(s):
+    def h(s):
+        return float(int(s, 16)) / 255
+
+    rgb = np.zeros((1, 1, 3))
+    rgb[0, 0, :] = [h(s[0:2]), h(s[2:4]), h(s[4:6])]
+    lab = color.rgb2lab(rgb)
+    return lab[0, 0, 0]
+
+
+def sort_with_order(vs, order):
+    r = np.zeros(len(order))
+    for i, o in enumerate(order):
+        r[i] = vs[o]
+    return r
+
+
+# chart_order = [23, 12, 9, 22, 0, 14, 7, 3, 21, 17, 16, 6, 5, 10, 15, 18]
+chart_l_values = map(extract_l, chart_colors_str.split("\n"))
+chart_order = [i[0] for i in sorted(enumerate(chart_l_values), key=lambda x:x[1])]
+print(list(enumerate(chart_order)))
+plt.plot(chart_l_values, marker='o')
+plt.show()
+ordered_chart_values = sort_with_order(chart_l_values, chart_order)
+plt.plot(ordered_chart_values, marker='o')
+plt.show()
 
 
 def get_image_path(movie, file):
@@ -96,7 +151,7 @@ class PointROI:
     def __getitem__(self, item):
         return self.points[item]
 
-    def rect(self):     # Return bounding rect.
+    def rect(self):  # Return bounding rect.
         xs = map(lambda a: a[0], self.points)
         ys = map(lambda a: a[1], self.points)
         x = min(xs)
@@ -129,11 +184,11 @@ def draw_cells(cellss, img=None, count=None):
 def get_cie_rois_custom(path, rois):
     img = io.imread(path)
     labs = []
-    img2 = np.zeros((240,10,3))
+    img2 = np.zeros((240, 10, 3))
     count = 0
     for roi in rois:
         rgb = img[roi[1]:roi[1] + roi[3], roi[0]:roi[0] + roi[2], :]
-        img2[count*10:count*10+roi[3],0:roi[2],:] = rgb
+        img2[count * 10:count * 10 + roi[3], 0:roi[2], :] = rgb
         lab = color.rgb2lab(rgb)
         labs.append(np.mean(lab, axis=(0, 1)))
         count += 1
@@ -142,21 +197,27 @@ def get_cie_rois_custom(path, rois):
     return np.array(labs)
 
 
+def extract_used_l(ls):
+    return [ls[23], ls[22], ls[21], ls[18]]
+
+
 def main():
     os.chdir(os.path.join(os.path.dirname(__file__), os.pardir))
     roiss = read_csv('parameters/20161013/calibration_rois.csv')
     count = 0
     img = np.zeros((240, 1000, 3))
     line_counts = []
+    lss = []
     for k, rois in roiss.iteritems():
         path = get_image_path(movie_names[int(k)], 1)
         cellss = map(mk_cells, rois)
-        print(zip(rois,cellss))
-        img2 = io.imread(path)
-        draw_cells(cellss, img2,count)
+        # print(zip(rois,cellss))
+        # img2 = io.imread(path)
+        # draw_cells(cellss, img2,count)
         for cells in cellss:
             ls = get_cie_rois(path, map(lambda cell: cell.values(), cells))
             # print(ls[:, 0])
+            lss.append(ls[:, 0])
             if np.isnan(ls[0, 0]):
                 print('NaN', movie_names[int(k)], cells)
             lab = np.zeros((240, 10, 3))
@@ -173,7 +234,20 @@ def main():
     img = img[:, 0:(count * 10), :]
     for i in line_counts:
         img[:, i * 10 - 1, :] = 1
-    plt.imshow(img)
+    # plt.imshow(img)
+    # plt.show()
+    lss = np.array(lss).transpose()
+    l_mean = np.mean(lss,axis=1)
+    chart_order = [i[0] for i in sorted(enumerate(l_mean), key=lambda x: x[1])]
+    lss2 = np.zeros((len(chart_order), lss.shape[1]))
+    for i in range(lss.shape[1]):
+        lss2[:, i] = sort_with_order(lss[:, i], chart_order)
+        # lss2[:, i] /= lss2[10, i]
+    plt.plot(lss)
+    np.savetxt('data/20161013 calibration l values.csv',lss.transpose())
+    plt.show()
+    # plt.plot(ordered_chart_values, linewidth=3)
+    plt.plot(lss2)
     plt.show()
 
 
