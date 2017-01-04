39cb9836132e3f6be4bf7e90226e0739a1ee630d
Hiro Kai <fullerenec84@gmail.com>
2017-01-04 10:58:06 +0900

    2nd and 3rd cycles CV

--- src/plot_cv.py
+++ src/plot_cv.py
@@ -4,7 +4,7 @@ import luigi
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import MultipleLocator
-
+from data_tools import colors10
 from luigi_tools import cleanup
 from src.figure_tools import set_common_format, plot_and_save
 
@@ -50,9 +50,9 @@ class PlotCV(luigi.Task):
         plot_and_save(plot_cv, self.name)
 
 
-def plot_cv2(only_last=True):
+def plot_cv2():
     fig, ax = plt.subplots(figsize=(4.5, 3))
-    path = os.path.join('../data/2000 rpm CV.txt')
+    path = os.path.join('../data/cv/1021/74 30wt 2krpm.txt')
     vs2 = []
     with open(path, 'rU') as f:
         s = ''
@@ -60,23 +60,23 @@ def plot_cv2(only_last=True):
             s = f.readline()
         f.readline()  # Skip a blank line
         for line in f:
-            ws = line.split('\t')
+            ws = line.split(', ')
             if len(ws) >= 2:
                 vs2.append(map(float, ws))
     arr = np.array(vs2)
     arr[:, 1] = arr[:, 1] * 1e6
 
     # Choose last cycle
-    if only_last:
-        idxs = np.argwhere(arr[:, 0] == 0)
-        idx1 = idxs[-3][0]
-        idx2 = idxs[-1][0]
-    else:
-        idx1 = 0
-        idx2 = -1
-
+    idxs = np.argwhere(arr[:, 0] == 0)
     # Potting part of split traces
-    plt.plot(arr[idx1:idx2, 0], arr[idx1:idx2, 1], c='b', lw=1)
+    idx1 = idxs[-5][0]
+    idx2 = idxs[-3][0]
+    plt.plot(arr[idx1:idx2, 0], arr[idx1:idx2, 1], c=colors10[0], lw=0.5)
+    idx1 = idxs[-3][0]
+    idx2 = idxs[-1][0]
+    plt.plot(arr[idx1:idx2, 0], arr[idx1:idx2, 1], c=colors10[1], lw=0.5)
+    # idx1 = 0
+    # idx2 = -1
 
     # Set range
     plt.xlim([-0.8, 1])
@@ -100,12 +100,16 @@ class PlotCV2(luigi.Task):
 
     def run(self):
         set_common_format()
+
+        # def plot_cv_all():
+        #     plot_cv2(only_last=False)
+        #
         plot_and_save(plot_cv2, self.name)
 
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
-    cleanup(PlotCV(name='2b'))
-    luigi.run(['PlotCV', '--name', '2b'])
-    cleanup(PlotCV(name='cv_revision'))
+    # cleanup(PlotCV(name='2b'))
+    # luigi.run(['PlotCV', '--name', '2b'])
+    cleanup(PlotCV2(name='cv_revision'))
     luigi.run(['PlotCV2', '--name', 'cv_revision'])
